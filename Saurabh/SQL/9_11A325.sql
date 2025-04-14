a2ae6bdbd19c4ed5287b59e56a2620e2514e7094
SaurHub007 <161135159+SaurHub007@users.noreply.github.com>
2025-04-14 11:31:54 +0530

    Add files via upload

---
+++ Saurabh/SQL/9_11A325.sql
@@ -0,0 +1,10 @@
+show databases;
+create database a325;
+show databases;
+use a325;
+create table student_info(
+roll_no tinyint,
+attendance bit,
+pocket_money smallint);
+show tables;
+describe student_info;
